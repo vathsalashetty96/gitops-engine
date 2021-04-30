4929ecac77fc740ba373fcaf191e493bba4bec07
vathsalashetty96 <82566301+vathsalashetty96@users.noreply.github.com>
2021-04-30 18:00:12 +0530

    Update waves_test.go

--- pkg/sync/syncwaves/waves_test.go
+++ pkg/sync/syncwaves/waves_test.go
@@ -5,11 +5,11 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	. "github.com/argoproj/gitops-engine/pkg/utils/testing"
+	. "github.com/vathsalashetty96/gitops-engine/pkg/utils/testing"
 )
 
 func TestWave(t *testing.T) {
 	assert.Equal(t, 0, Wave(NewPod()))
-	assert.Equal(t, 1, Wave(Annotate(NewPod(), "argocd.argoproj.io/sync-wave", "1")))
+	assert.Equal(t, 1, Wave(Annotate(NewPod(), "argocd.vathsalashetty96.io/sync-wave", "1")))
 	assert.Equal(t, 1, Wave(Annotate(NewPod(), "helm.sh/hook-weight", "1")))
 }
